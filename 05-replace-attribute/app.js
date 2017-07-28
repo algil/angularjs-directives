03900e43c9adf0ec2768f42e084d6e7728210017
Antonio Gil <antonio.luis.gil@gmail.com>
2017-07-28 10:09:36 +0200

    Using replace attribute 'to hide' custom tags

---
+++ 05-replace-attribute/app.js
@@ -0,0 +1,28 @@
+angular.module('app', []);
+
+angular.module('app').controller('MainController', function() {
+  var vm = this;
+  vm.user = {
+    name: 'Antonio',
+    address: {
+      city: 'Malaga',
+      country: 'Spain'
+    },
+    friends: [
+        'Sergio',
+        'Inma',
+        'Izan'
+    ]
+  }
+});
+
+angular.module('app').directive('userInfoCard', function () {
+  return {
+    templateUrl: 'userInfoCard.html',
+    restrict: 'E',
+    //This attribute replace custom tags element (<user-info-card>) for a valid html tags (<div >)
+    //If this is true, template only must have a root element
+    //NOTE: It's not a good practice to use replace attribute
+    replace: true
+  }
+});
