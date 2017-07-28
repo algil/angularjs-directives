ea1f2e943d067f6b6d8c3b59408d062692605a33
Antonio Gil <antonio.luis.gil@gmail.com>
2017-07-28 09:14:42 +0200

    Created first basic directive example

--- 01-first-directive/app.js
+++ 01-first-directive/app.js
@@ -1,12 +1,12 @@
 angular.module('app', []);
 
-angular.module('app').controller('MainController', MainController);
-
-
-function MainController($scope) {
+angular.module('app').controller('MainController', function($scope) {
   var vm = this;
+});
 
-
-
-
-}
+angular.module('app').directive('userInfoCard', function () {
+  return {
+    template: "User Info Card",
+    restrict: "E"
+  }
+});
